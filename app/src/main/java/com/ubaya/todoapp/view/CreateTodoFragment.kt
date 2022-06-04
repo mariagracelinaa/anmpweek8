8f1e83de84a0870435dde0b1888740fb49a4bfe6
mariagracelinaa <gracezhouu@gmail.com>
2022-06-04 22:26:16 +0700

    init kelas w12 (2, sudah selesai tapi belum di run)

--- app/src/main/java/com/ubaya/todoapp/view/CreateTodoFragment.kt
+++ app/src/main/java/com/ubaya/todoapp/view/CreateTodoFragment.kt
@@ -1,11 +1,16 @@
 package com.ubaya.todoapp.view
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.RadioButton
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
@@ -19,11 +24,20 @@ import com.ubaya.todoapp.util.NotificationHelper
 import com.ubaya.todoapp.util.TodoWorker
 import com.ubaya.todoapp.viewmodel.DetailTodoViewModel
 import kotlinx.android.synthetic.main.fragment_create_todo.*
+import kotlinx.android.synthetic.main.fragment_create_todo.view.*
+import java.util.*
 import java.util.concurrent.TimeUnit
 
-class CreateTodoFragment : Fragment(), TodoAddListener, TodoPriorityClickListener, TodoDateListener {
+class CreateTodoFragment : Fragment(), TodoAddListener, TodoPriorityClickListener, TodoDateListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     private lateinit var viewModel:DetailTodoViewModel
     private lateinit var dataBinding: FragmentCreateTodoBinding
+
+    //Variable untuk perhitungan waktu tunda notifikasi
+    var year = 0
+    var month = 0
+    var day = 0
+    var hour = 0
+    var minute = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -79,13 +93,18 @@ class CreateTodoFragment : Fragment(), TodoAddListener, TodoPriorityClickListene
     }
 
     override fun onAddNewTodo(view: View) {
+        //Hitung selisih waktu sekarang dengan tenggat
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day, hour, minute, 0)
+        val today = Calendar.getInstance()
+        val diff = calendar.timeInMillis / 1000L - today.timeInMillis / 1000L
         dataBinding.todo?.let {
             val list = listOf(it)
             viewModel.addTodo(list)
             Toast.makeText(view.context, "Todo created", Toast.LENGTH_LONG).show()
             Navigation.findNavController(view).popBackStack()
             val myWorkRequest = OneTimeWorkRequestBuilder<TodoWorker>()
-                .setInitialDelay(5, TimeUnit.SECONDS)
+                .setInitialDelay(diff, TimeUnit.SECONDS)
                 .setInputData(
                     workDataOf(
                         "title" to "Todo ${it.title} created",
@@ -96,11 +115,39 @@ class CreateTodoFragment : Fragment(), TodoAddListener, TodoPriorityClickListene
         }
     }
 
+    //Untuk memunculkan datetime picker dgn tgl skrg
     override fun onDateClick(view: View) {
-        //slide 69
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        activity?.let{
+            DatePickerDialog(it, this, year, month, day).show()
+        }
     }
 
     override fun onTimeClick(view: View) {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity)).show()
+    }
 
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        Calendar.getInstance().let {
+            it.set(year, month, day)
+            val date = day.toString().padStart(2, '0') + '-' + month.toString().padStart(2, '0') + '-' + year.toString().padStart(2, '0')
+            dataBinding.root.txtDate.setText(date)
+
+            this.year = year
+            this.month = month
+            this.day = day
+        }
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        val time = hourOfDay.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0')
+        this.hour = hourOfDay
+        this.minute = minute
     }
 }
